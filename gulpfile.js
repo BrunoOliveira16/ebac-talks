4548c3d708bb8c686840683d846ddb0f975d69fe
Bruno <bruno1_oliveira@hotmail.com>
2022-08-28 18:58:56 -0300

    Etapa 04 - concluido minificação e alterado src do index.html e efetuado testes de funcionalidades

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,18 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+/*const image = require('gulp-image')*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
 
 function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src([
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+            './vendor/owl/css/owl.css',
+            './vendor/jquery-ui/jquery-ui.css',
+            './src/css/style.css'
+        ])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
@@ -15,12 +22,39 @@ function tarefasCSS(cb) {
 
 function tarefasJS() {
 
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src([
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './vendor/owl/js/owl.js',
+            './vendor/jquery-mask/jquery.mask.js',
+            './vendor/jquery-ui/jquery-ui.js',
+            './src/js/custom.js'
+        ])
+        .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'})) // libs.min.js
         .pipe(gulp.dest('./dist/js'))
 }
 
+/*function tarefasImagem() {
+
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress:false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+
+}*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+
+/*exports.images = tarefasImagem*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
