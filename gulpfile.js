dc089010d61ccad14fb3a9ac29c3a26abdb3b25a
Bruno <bruno1_oliveira@hotmail.com>
2022-08-31 06:39:33 -0300

    Etapa 07 - atualização gulpfile inserindo código de minificação para html

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 /*const image = require('gulp-image')*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
+const stripCss = require('gulp-strip-css-comments')
+const stripJs = require('gulp-strip-comments')
+const htmlmin = require('gulp-htmlmin')
+const { series } = require('gulp')
 
 function tarefasCSS(cb) {
 
@@ -14,10 +18,11 @@ function tarefasCSS(cb) {
             './vendor/jquery-ui/jquery-ui.css',
             './src/css/style.css'
         ])
-        .pipe(concat('styles.css'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min'})) // libs.min.css
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(stripCss())                // remove comentarios
+        .pipe(concat('styles.css'))      // mescla arquivos
+        .pipe(cssmin())                  // minifica css
+        .pipe(rename({ suffix: '.min'})) // styles.min.css
+        .pipe(gulp.dest('./dist/css'))   // cria arquivo em novo diretório
 }
 
 function tarefasJS() {
@@ -30,10 +35,21 @@ function tarefasJS() {
             './vendor/jquery-ui/jquery-ui.js',
             './src/js/custom.js'
         ])
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min'})) // libs.min.js
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(stripJs())                 // remove comentarios 
+        .pipe(concat('scripts.js'))      // mescla arquivos
+        .pipe(uglify())                  // minifica Js
+        .pipe(rename({ suffix: '.min'})) // scripts.min.js
+        .pipe(gulp.dest('./dist/js'))    // cria arquivo em novo diretório
+}
+
+// POC - Proof of concept
+function tarefasHTML(callback){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))       // cria arquivo em novo diretório
+
+    return callback()
 }
 
 /*function tarefasImagem() {
@@ -54,7 +70,8 @@ function tarefasJS() {
 
 }*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
 
+exports.default = series( tarefasHTML, tarefasJS, tarefasCSS)
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 
-/*exports.images = tarefasImagem*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
+/*exports.images = tarefasImagem*/ /* Necessário verificar pois está dando erro ao tentar compactar as imagens */
