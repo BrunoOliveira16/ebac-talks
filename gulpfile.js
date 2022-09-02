40e938501b9f9eb4d9ca0a96487d0978faa43ab3
Bruno <bruno1_oliveira@hotmail.com>
2022-09-01 22:18:14 -0300

    atualização arquivo gulpfile, instalação babel, browser-sync e minificação do arquivo html

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,38 @@
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-/*const image = require('gulp-image')*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
+const imagemin = require('gulp-imagemin')
 const stripCss = require('gulp-strip-css-comments')
 const stripJs = require('gulp-strip-comments')
 const htmlmin = require('gulp-htmlmin')
-const { series } = require('gulp')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 function tarefasCSS(cb) {
 
-    return gulp.src([
+/*return*/  gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             './vendor/owl/css/owl.css',
             './vendor/jquery-ui/jquery-ui.css',
             './src/css/style.css'
         ])
-        .pipe(stripCss())                // remove comentarios
+        .pipe(stripCss())                 // remove comentários css   
         .pipe(concat('styles.css'))      // mescla arquivos
         .pipe(cssmin())                  // minifica css
         .pipe(rename({ suffix: '.min'})) // styles.min.css
         .pipe(gulp.dest('./dist/css'))   // cria arquivo em novo diretório
+
+    cb()
 }
 
-function tarefasJS() {
+function tarefasJS(callback) {
 
-    return gulp.src([
+/*return*/  gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
@@ -35,11 +40,27 @@ function tarefasJS() {
             './vendor/jquery-ui/jquery-ui.js',
             './src/js/custom.js'
         ])
-        .pipe(stripJs())                 // remove comentarios 
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))                               // remove comentários Js
         .pipe(concat('scripts.js'))      // mescla arquivos
         .pipe(uglify())                  // minifica Js
         .pipe(rename({ suffix: '.min'})) // scripts.min.js
         .pipe(gulp.dest('./dist/js'))    // cria arquivo em novo diretório
+
+    return callback()
+}
+
+function tarefasImagem(cb){
+
+    gulp.src('src/images/*.{png,jpg,jpeg}')
+        .pipe(imagemin({
+            verbose: true
+        }))
+        .pipe(dest('./dist/images'))
+
+    cb()
 }
 
 // POC - Proof of concept
@@ -52,26 +73,19 @@ function tarefasHTML(callback){
     return callback()
 }
 
-/*function tarefasImagem() {
+gulp.task('serve', function(){
 
-    return gulp.src('./src/images/*')
-        .pipe(image({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress:false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true
-        }))
-        .pipe(gulp.dest('./dist/images'))
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./dist/**/*').on('change', reload)
 
-}*/ /* Necessário verificar pois está dando erro ao tenatr compactar as iamgens */
+})
 
-exports.default = series( tarefasHTML, tarefasJS, tarefasCSS)
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefasImagem
 
-/*exports.images = tarefasImagem*/ /* Necessário verificar pois está dando erro ao tentar compactar as imagens */
+exports.default = parallel( tarefasHTML, tarefasJS, tarefasCSS)
